67bb28fb85fe10f0b8655d5d6f7353fbc5ad5f91
zaneles333 <selena.zhang.333@gmail.com>
2025-12-03 14:38:32 -0800

    Input structure complete. Times not randomized, categoricals not vectorized. See fall oral presentation for status

---
+++ preprocessing-structured/main.py
@@ -0,0 +1,23 @@
+print("Starting session...");
+
+import csv
+
+# load csv files
+
+
+
+# organize csv data by hadm_id - hospital stay id
+
+    # need to decide which parameters to keep
+
+
+
+# time bins
+
+
+
+# output into some sort of csv format
+
+
+
+
